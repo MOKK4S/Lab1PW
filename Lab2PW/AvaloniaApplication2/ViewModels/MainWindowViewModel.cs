d01ff9b61c90c6986fe33a7729fd26e27a41207e
Kacper Gałaj <72792749+MOKK4S@users.noreply.github.com>
2026-03-13 18:52:00 +0100

    add main window

--- Lab2PW/AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
+++ Lab2PW/AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace AvaloniaApplication2.ViewModels;
+using System.Collections.ObjectModel;
+using AvaloniaApplication2.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaApplication2.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private decimal _totalPrice;
+
+    public ObservableCollection<Product> Cart { get; } = new();
+
+    public System.Action? RequestProductSelection { get; set; }
+    public System.Action? RequestTransportSelection { get; set; }
+    public System.Action? RequestPaymentSelection { get; set; }
+
+    [RelayCommand]
+    private void OpenProductSelection()
+    {
+        RequestProductSelection?.Invoke();
+    }
+
+    [RelayCommand]
+    private void OpenTransportSelection()
+    {
+        RequestTransportSelection?.Invoke();
+    }
+
+    [RelayCommand]
+    private void OpenPaymentSelection()
+    {
+        RequestPaymentSelection?.Invoke();
+    }
+
+    [RelayCommand]
+    private void ClearCart()
+    {
+        Cart.Clear();
+        TotalPrice = 0;
+    }
 }
--- Lab2PW/AvaloniaApplication2/Views/MainWindow.axaml.cs
+++ Lab2PW/AvaloniaApplication2/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvaloniaApplication2.ViewModels;
 
 namespace AvaloniaApplication2.Views;
 
@@ -7,5 +9,58 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
+        
+        if (DataContext is MainWindowViewModel vm)
+        {
+            vm.RequestProductSelection = OpenProductSelection;
+            vm.RequestTransportSelection = OpenTransportSelection;
+            vm.RequestPaymentSelection = OpenPaymentSelection;
+        }
+    }
+
+    public async void OpenPaymentSelection()
+    {
+        if (DataContext is MainWindowViewModel mainVm)
+        {
+            var dialog = new PaymentWindow(mainVm.TotalPrice);
+            var result = await dialog.ShowDialog<bool>(this);
+            if (result)
+            {
+                mainVm.Cart.Clear();
+                mainVm.TotalPrice = 0;
+            }
+        }
+    }
+
+    public async void OpenTransportSelection()
+    {
+        var dialog = new TransportWindow();
+        await dialog.ShowDialog<bool>(this);
+        
+        if (DataContext is MainWindowViewModel mainVm)
+        {
+            mainVm.TotalPrice += dialog.TransportCost;
+        }
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public async void OpenProductSelection()
+    {
+        var dialog = new ProductSelectionWindow();
+        var result = await dialog.ShowDialog<bool>(this);
+
+        if (result && dialog.DataContext is ProductSelectionViewModel vm && vm.SelectedProduct != null)
+        {
+            if (DataContext is MainWindowViewModel mainVm)
+            {
+                mainVm.Cart.Add(vm.SelectedProduct);
+                mainVm.TotalPrice += vm.SelectedProduct.Price;
+            }
+        }
     }
 }
