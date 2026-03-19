45746eb1a09ba52ab2cfdd03fb3c2c16ef451e10
Kacper Gałaj <72792749+MOKK4S@users.noreply.github.com>
2026-03-19 14:00:00 +0100

    add employee model

---
+++ Lab3PW/Lab3PW/Lab3PW/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace Lab3PW.Models;
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Position { get; set; } = string.Empty;
+}
