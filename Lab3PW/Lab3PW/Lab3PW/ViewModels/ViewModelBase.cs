2548517a3deccda16e84a633ea2f8393053e5004
Kacper Gałaj <72792749+MOKK4S@users.noreply.github.com>
2026-03-19 13:39:09 +0100

    3lab project init

---
+++ Lab3PW/Lab3PW/Lab3PW/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Lab3PW.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Lab3PW/Lab3PW/Lab3PW/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Lab3PW.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
