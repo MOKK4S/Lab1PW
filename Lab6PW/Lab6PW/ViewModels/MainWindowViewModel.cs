d2b227fb40448519d2c5b153050545c7ab2639c9
Kacper Gałaj <72792749+MOKK4S@users.noreply.github.com>
2026-04-23 16:32:00 +0200

    main menu ui

--- Lab6PW/Lab6PW/ViewModels/MainWindowViewModel.cs
+++ Lab6PW/Lab6PW/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Lab6PW.ViewModels;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lab6PW.Models;
+
+namespace Lab6PW.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] private int _boardX = 3;
+    [ObservableProperty] private int _boardY = 3;
+    [ObservableProperty] private int _hyraxCount = 1;
+    [ObservableProperty] private int _raccoonCount = 2;
+    [ObservableProperty] private int _crocodileCount = 0;
+    [ObservableProperty] private bool _isSettingsVisible = false;
+    public ObservableCollection<ScoreEntry> Scores { get; } = new();
+
+    [RelayCommand] private void ToggleSettings() => IsSettingsVisible = !IsSettingsVisible;
+    [RelayCommand] private void Exit() => System.Environment.Exit(0);
 }
