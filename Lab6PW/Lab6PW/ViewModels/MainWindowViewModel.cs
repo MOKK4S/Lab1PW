41f2a933056781124dd937047f9a779615400fee
Kacper Gałaj <72792749+MOKK4S@users.noreply.github.com>
2026-04-23 15:14:00 +0200

    init

---
+++ Lab6PW/Lab6PW/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab6PW.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Lab6PW/Lab6PW/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Lab6PW.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
